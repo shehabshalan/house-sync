a53f06aaa027bf3647f7855db1251cd6ae307cee
shehabshalan <shehab.shalan@hotmail.com>
2023-12-25 20:01:14 +0100

    chore: Add planed empty endpoints todo

--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from app.config import settings
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -8,6 +9,76 @@ app = FastAPI()
 @app.get("/healthcheck")
 async def healthcheck():
     return {"status": "ok"}
+
+
+@app.get("/space")
+async def get_spaces(arg):
+    pass
+
+
+@app.get("/space/{space_id}")
+async def get_space(space_id):
+    pass
+
+
+@app.post("/space")
+async def create_space():
+    pass
+
+
+@app.put("/space/{space_id}")
+async def update_space(space_id):
+    pass
+
+
+@app.delete("/space/{space_id}")
+async def delete_space(space_id):
+    pass
+
+
+@app.get("/space/{space_id}/task")
+async def get_space_task(space_id):
+    pass
+
+
+@app.post("/space/{space_id}/task")
+async def create_space_task(space_id):
+    pass
+
+
+@app.put("/space/{space_id}/task/{task_id}")
+async def update_space_task(space_id):
+    pass
+
+
+@app.delete("/space/{space_id}/task/{task_id}")
+async def delete_space_task(space_id):
+    pass
+
+
+@app.get("/space/{space_id}/inventory")
+async def get_space_inventory(space_id):
+    pass
+
+
+@app.post("/space/{space_id}/inventory")
+async def create_space_inventory(space_id):
+    pass
+
+
+@app.put("/space/{space_id}/inventory/{inventory_id}")
+async def update_space_inventory(space_id):
+    pass
+
+
+@app.delete("/space/{space_id}/inventory/{inventory_id}")
+async def delete_space_inventory(space_id):
+    pass
+
+
+@app.get("/user/{user_id}")
+async def get_user(user_id):
+    pass
 
 
 def main():
