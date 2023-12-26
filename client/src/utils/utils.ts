8b4625f636161a5ff1d95929b710a1792b88cc80
shehabshalan <shehab.shalan@hotmail.com>
2023-12-26 21:18:56 +0100

    feat: Add utils.ts file with token handling and Google Auth initialization

---
+++ client/src/utils/utils.ts
@@ -0,0 +1,23 @@
+export const getToken = () => {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+  return token;
+};
+
+export const handleCallbackResponse = (response: any) => {
+  const token = response.credential;
+  return token;
+};
+export const initGoogleAuth = async (callback: (response: any) => void) => {
+  window.google.accounts.id.initialize({
+    client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    callback: callback,
+  });
+
+  window.google.accounts.id.renderButton(document.getElementById("login-div"), {
+    theme: "outline",
+    size: "large",
+  });
+
+  // window.google.accounts.id.prompt();
+};
