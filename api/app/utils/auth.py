ff6fd23cd9529188637fe3256bcb310db8b93f41
shehabshalan <shehab.shalan@hotmail.com>
2023-12-27 00:57:08 +0100

    chore: handle google client id error

--- api/app/utils/auth.py
+++ api/app/utils/auth.py
@@ -19,6 +19,7 @@ def generate_access_token(data: dict):
     try:
         if not SECRET_KEY:
             raise ValueError("No secret key configured for JWT")
+
         to_encode = data.copy()
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
         to_encode.update({"exp": expire})
@@ -30,6 +31,8 @@ def generate_access_token(data: dict):
 
 def verify_google_token(token: str):
     try:
+        if not CLIENT_ID:
+            raise ValueError("No client ID configured for Google OAuth")
         id_info = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
         return id_info
     except ValueError:
