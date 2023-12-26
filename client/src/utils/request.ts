4136fc9f5f13a11e436c66bc003df80d3c32f666
shehabshalan <shehab.shalan@hotmail.com>
2023-12-26 21:17:08 +0100

    feat: setup api endpoints and axios instance

---
+++ client/src/utils/request.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { getToken } from "./utils";
+
+const BASE_URL_PROD = import.meta.env.VITE_PROD_BASE_URL;
+const BASE_URL_DEV = import.meta.env.VITE_DEV_BASE_URL;
+const ENV = import.meta.env.NODE_ENV;
+const BASE_URL = ENV === "production" ? BASE_URL_PROD : BASE_URL_DEV;
+
+const request = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+request.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) config.headers["Authorization"] = token;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default request;
