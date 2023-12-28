004c918c96657b77231f327b7106ab88f473e82b
shehabshalan <shehab.shalan@hotmail.com>
2023-12-28 03:04:56 +0100

    feat: Add Task and TaskUser models

--- api/app/models.py
+++ api/app/models.py
@@ -1,6 +1,7 @@
+import enum
 from typing import List, Optional
 
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Column, Enum, Field, Relationship, SQLModel
 
 
 class User(SQLModel, table=True):
@@ -9,6 +10,11 @@ class User(SQLModel, table=True):
     picture: Optional[str] = None
     is_active: bool = False
     spaces: List["Space"] = Relationship(back_populates="users")
+
+
+class TaskFrequency(str, enum.Enum):
+    WEEKLY = "WEEKLY"
+    MONTHLY = "MONTHLY"
 
 
 class Space(SQLModel, table=True):
@@ -22,3 +28,19 @@ class Space(SQLModel, table=True):
 class SpaceUser(SQLModel, table=True):
     space_id: int = Field(primary_key=True, default=None, foreign_key="space.id")
     user_email: str = Field(primary_key=True, default=None, foreign_key="user.email")
+
+
+class Task(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, index=True)
+    name: str
+    description: str
+    frequency: TaskFrequency = Field(sa_column=Column(Enum(TaskFrequency)))
+    space_id: int = Field(default=None, foreign_key="space.id")
+
+
+class TaskUser(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, index=True)
+    task_id: int = Field(default=None, foreign_key="task.id")
+    user_email: str = Field(default=None, foreign_key="user.email")
+    is_completed: bool = False
+    due_date: Optional[str] = None
