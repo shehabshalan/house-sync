912bd054c16f31c78e34c7cc682de575689c51f8
shehabshalan <shehab.shalan@hotmail.com>
2023-12-28 03:09:08 +0100

    chore: Add task functionality and invite members button to SpaceInformation component

--- client/src/components/SpaceInformation.tsx
+++ client/src/components/SpaceInformation.tsx
@@ -1,30 +1,40 @@
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import AddTask from "./AddTask";
+import { Mail } from "lucide-react";
 
-const SpaceInformation = () => {
+type SpaceInformationProps = {
+  space: SpaceDetails | undefined;
+};
+
+const SpaceInformation = ({ space }: SpaceInformationProps) => {
+  const { name, users } = space || {};
   return (
     <Card>
       <CardHeader className="flex justify-between flex-row">
-        <CardTitle>Space Name</CardTitle>
-        <Button size="sm" variant="outline">
-          Edit Space
-        </Button>
+        <CardTitle>{name}</CardTitle>
+        <div className="flex items-center gap-2">
+          <Button size="sm" variant="outline">
+            Edit Space
+          </Button>
+          <Button size="sm" variant="secondary">
+            <Mail className="mr-2 h-4 w-4" /> Invite Members
+          </Button>
+          <AddTask />
+        </div>
       </CardHeader>
       <CardContent className="flex justify-between items-center">
         <div className="flex items-center -space-x-1">
-          <Avatar>
-            <AvatarImage alt="Member 1" src="/placeholder-avatar.jpg" />
-            <AvatarFallback>M1</AvatarFallback>
-          </Avatar>
-          <Avatar>
-            <AvatarImage alt="Member 2" src="/placeholder-avatar.jpg" />
-            <AvatarFallback>M2</AvatarFallback>
-          </Avatar>
-          <Avatar>
-            <AvatarImage alt="Member 3" src="/placeholder-avatar.jpg" />
-            <AvatarFallback>M3</AvatarFallback>
-          </Avatar>
+          {users?.slice(0, 5).map((user) => (
+            <Avatar key={user.email}>
+              <AvatarImage
+                alt={user.name}
+                src={user.picture || "/placeholder-avatar.jpg"}
+              />
+              <AvatarFallback>{user.name}</AvatarFallback>
+            </Avatar>
+          ))}
         </div>
       </CardContent>
     </Card>
