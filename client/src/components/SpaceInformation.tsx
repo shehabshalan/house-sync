283c0b0734e483348c14e41fb7105f75dcb3869d
shehabshalan <shehab.shalan@hotmail.com>
2023-12-28 17:05:08 +0100

    chore: [wip] Add delete functionality to SpaceInformation component

--- client/src/components/SpaceInformation.tsx
+++ client/src/components/SpaceInformation.tsx
@@ -2,14 +2,42 @@ import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import AddTask from "./AddTask";
-import { Mail } from "lucide-react";
+import { Mail, TrashIcon } from "lucide-react";
+import { ActionDialog } from "./ActionDialog";
+import { useDeleteSpace } from "@/services/useDeleteSpace";
+import { useToast } from "./ui/use-toast";
+import { useQueryClient } from "@tanstack/react-query";
 
 type SpaceInformationProps = {
   space: SpaceDetails | undefined;
 };
 
 const SpaceInformation = ({ space }: SpaceInformationProps) => {
-  const { name, users } = space || {};
+  const { name, users, id } = space || {};
+  const { mutate } = useDeleteSpace();
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+  const handleDelete = () => {
+    console.log("Delete space", id);
+
+    mutate(Number(id), {
+      onSuccess: () => {
+        toast({
+          variant: "default",
+          title: "Space Deleted",
+        });
+        queryClient.invalidateQueries({ queryKey: ["spaces"] });
+        console.log("Space deleted");
+      },
+      onError: (e: Error & { response?: any }) => {
+        toast({
+          variant: "destructive",
+          title: e.response?.data?.detail || "An error occurred",
+          description: "There was a problem with your request. Try again.",
+        });
+      },
+    });
+  };
   return (
     <Card>
       <CardHeader className="flex justify-between flex-row">
@@ -22,6 +50,17 @@ const SpaceInformation = ({ space }: SpaceInformationProps) => {
             <Mail className="mr-2 h-4 w-4" /> Invite Members
           </Button>
           <AddTask />
+          <ActionDialog
+            title="Delete Space"
+            description="Are you sure you want to delete this space?"
+            onContinue={handleDelete}
+            continueButtonText="Delete"
+            cancelButtonText="Cancel"
+          >
+            <Button variant="destructive" size="icon" className="ml-auto">
+              <TrashIcon className="w-4 h-4" />
+            </Button>
+          </ActionDialog>
         </div>
       </CardHeader>
       <CardContent className="flex justify-between items-center">
