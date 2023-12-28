c18264708df9d356ddbb6a3d9561da3be834ed4b
shehabshalan <shehab.shalan@hotmail.com>
2023-12-28 03:06:32 +0100

    feat: Add new types for SpaceDetails, UserTask, and Task

--- client/src/utils/types.ts
+++ client/src/utils/types.ts
@@ -1,3 +1,8 @@
+enum Frequency {
+  WEEKLY = "WEEKLY",
+  MONTHLY = "MONTHLY",
+}
+
 type User = {
   name: string;
   email: string;
@@ -5,9 +10,29 @@ type User = {
   token?: string;
 };
 
-type Space = {
-  id: string;
+interface Space {
+  id: number;
   name: string;
   description: string;
   users: User[];
-};
+}
+
+interface UserTask {
+  due_date: string;
+  id: number;
+  is_completed: boolean;
+  task_id: number;
+  user_email: string;
+}
+interface Task {
+  id: string;
+  name: string;
+  description: string;
+  space_id: number;
+  frequency: Frequency;
+  users: UserTask[];
+}
+
+interface SpaceDetails extends Space {
+  tasks: Task[];
+}
