1afa2710bb18394984b0eaab7a95db19a62c0599
shehabshalan <shehab.shalan@hotmail.com>
2023-12-31 16:50:04 +0100

    chore: Update environment variable in request.ts

--- client/src/utils/request.ts
+++ client/src/utils/request.ts
@@ -3,7 +3,7 @@ import { getToken } from "./utils";
 
 const BASE_URL_PROD = import.meta.env.VITE_PROD_BASE_URL;
 const BASE_URL_DEV = import.meta.env.VITE_DEV_BASE_URL;
-const ENV = import.meta.env.NODE_ENV;
+const ENV = import.meta.env.MODE;
 const BASE_URL = ENV === "production" ? BASE_URL_PROD : BASE_URL_DEV;
 
 const request = axios.create({
